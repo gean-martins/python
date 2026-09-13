014c7b1d3d8287bc48718992a7928d10bb7d269e
Gean Carlos Alves Martins <dev.geanmartins@gmail.com>
2026-09-13 00:19:40 -0300

    Adiciona o arquivo "area_do_circulo.py" ao repo

---
+++ area_do_circulo.py
@@ -0,0 +1,9 @@
+#recebe o raio de uma circunferência, calcula e armazena a área do circulo na variável "area", exibe o resultado na tela
+
+PI = 3.14159
+
+raio = float(input())
+
+area = raio * raio * PI
+
+print(f"A={area}")
