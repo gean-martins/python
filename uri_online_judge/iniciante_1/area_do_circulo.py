b339a7b7843628c686a8a312eb8a982948c68344
Gean Carlos Alves Martins <dev.geanmartins@gmail.com>
2026-09-13 21:45:39 -0300

    move o "area_do_circulo" e o "somando_numeros" para dentro de ./uri_online_judge/iniciante_1
    - Serve para separar o que são códigos que fiz para treinar (URI online judge) dos códigos da matéria de "introdução a programação"
    - A subpasta "iniciante_1" armazena somente os códigos da categoria iniciante, separarei os códigos por categorias de problemas, assim como é separado no URI

---
+++ uri_online_judge/iniciante_1/area_do_circulo.py
@@ -0,0 +1,11 @@
+#recebe o raio de uma circunferência, calcula e armazena a área do circulo na variável "area", exibe o resultado na tela
+
+#[ERRO] - o site a beecrowd não aceita a solução, pois o site espera a saída com menos casas decimais e com arredondamentos, tem que dar uma olhada nisso...
+
+PI = 3.14159
+
+raio = float(input())
+
+area = raio * raio * PI
+
+print(f"A={area}")
