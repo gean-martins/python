edad21c2773b0b6ed286bdfea6bea38b8814af4f
Gean Carlos Alves Martins <dev.geanmartins@gmail.com>
2026-09-17 16:22:57 -0300

    Adiciona o arquivo polegadaPcentimetros a aula01

---
+++ introducao_a_programacao/aula01/polegadaPcentimetros.py
@@ -0,0 +1,11 @@
+#converte uma medida em polegada para centimetros
+
+#1 polegada = 2,54cm
+polegadaEmCentimetros = 2.54
+
+#medida em polegadas que quero converter
+polegada = 10
+
+resultado = polegada * polegadaEmCentimetros
+
+print(f"A medida em centímetros: {resultado}")
