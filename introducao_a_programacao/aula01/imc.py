c4f004db91b880797ece25a4ab313965d1197570
Gean Carlos Alves Martins <dev.geanmartins@gmail.com>
2026-09-17 16:35:15 -0300

    Adiciona o imc.py a aula01

---
+++ introducao_a_programacao/aula01/imc.py
@@ -0,0 +1,8 @@
+#calcula o IMC
+
+peso = 75
+altura = 1.75
+
+imc = peso / altura ** 2 # "**" é operador de potenciação, e é executado antes da multiplicação e divisão
+
+print(f"Valor do IMC: {imc}")
