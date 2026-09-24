17a1c7ef71adf8b0a82b5f9fd3610af8941c1f11
Gean Carlos Alves Martins <dev.geanmartins@gmail.com>
2026-09-24 15:37:10 -0300

    WIP - Adiciona o arquivo "contasComMultas.py" a aula02
    - Cria as variáveis iniciais
    - Tem que terminar o código

---
+++ introducao_a_programacao/aula02/contasComMultas.py
@@ -0,0 +1,6 @@
+#fulano recebeu seu salário e precisa pagar duas contas que estão atrasadas, por conta do atraso, fulano tem que pagar multa de 2% de cada conta, calcula e mostre quanto vai sobrar do salário de fulano
+
+salarioAntigo = float(input("Digite o seu salário: "))
+valorAntigoConta1 = float(input("Digite o valor a pagar da conta 1: "))
+valorAntigoConta2 = float(input("Digite o valor a pagar da conta 2: "))
+
