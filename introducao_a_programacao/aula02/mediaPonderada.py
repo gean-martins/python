e7d1da84302031e70e042c623468e0613d4686f1
Gean Carlos Alves Martins <dev.geanmartins@gmail.com>
2026-09-24 14:36:41 -0300

    Modifica para que o usuário possa digitar as notas durante a execução do programa

--- introducao_a_programacao/aula02/mediaPonderada.py
+++ introducao_a_programacao/aula02/mediaPonderada.py
@@ -1,8 +1,8 @@
 #faça um programa que receba duas notas, calcule e mostre a média ponderada dessas notas, considerando peso 2 para a primeira nota e peso 3 para a segunda nota
 
 #notas do aluno
-nota1 = 6
-nota2 = 6
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
 
 #pessos de cada nota
 peso1 = 2
